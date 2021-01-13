4b59c0d8d5d02b674c5bc07bca4332840c9ed068
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-01-13 12:58:43 -0500

    Creating a Delete crud function with route, controllers, actions, api, and reducer

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,5 @@
 
-import pkg from 'mongoose';
-const { mongoose } = pkg
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -33,9 +32,19 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body;
 
-    if(!mongoose.Types.ObjectId,isValid(_id)) return res.status(404).send('No post with that ID');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id,{ ...post, _id }, { new: true });
 
     res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that ID found');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post Deleted !' });
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,13 +3,16 @@ import express from 'express';
 // const Post = require('../models/postMessage');
 // const uploader = require('../config/cloudinary-setup')
 
-import { getPosts, createPost, updatePost } from '../controllers/posts.js'
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+
+
 // router.post('/', uploader.single('file'), (req, res) => {
 //     res.send("ok")
 // })
