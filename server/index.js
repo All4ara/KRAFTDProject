0b6ca58031240a7a91b0dae6df6e0abb0664a061
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-02-02 15:53:21 -0500

    refactor

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from  'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,10 +16,10 @@ app.use('/posts', postRoutes)
 
 
 
-const CONNECTION_URL = 'mongodb+srv://Ariel:ar3l1994@cluster0.rcjei.mongodb.net/MyData?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://Ariel:ar3l1994@cluster0.rcjei.mongodb.net/MyData?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
     .catch((error) => console.log(error.message));
 
