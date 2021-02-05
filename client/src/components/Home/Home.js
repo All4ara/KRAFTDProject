d21482e153d81edb1703cc911db81ff9e2f8fbff
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-02-05 15:31:19 -0500

    User Form

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
     const [currentId, setCurrentId] = useState(null)
     const dispatch = useDispatch();
-    // const classes = useStyles();
+    
 
     useEffect(() =>{
         dispatch(getPosts());
@@ -21,7 +21,7 @@ const Home = () => {
     return(
         <Grow in>
         <Container>
-          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
               <Grid item xs={12} sm={7}>
                   <Posts setCurrentId={setCurrentId}/>
               </Grid>
