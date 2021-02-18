660bb13c368617ef780bfd66c3c4a965499809c7
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-02-18 18:05:29 -0500

    Post show most recent first

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });  //https://kraftd.herokuapp.com
+const API = axios.create({ baseURL: 'https://kraftd.herokuapp.com' });  //https://kraftd.herokuapp.com or http://localhost:5000
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
