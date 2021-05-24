d949f926a00720aad0aed1a8f6995eaebc75445f
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-05-24 16:38:26 -0400

    set up api reqests for search with tags

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,11 +3,12 @@ import express from 'express';
 // const Post = require('../models/postMessage');
 // const uploader = require('../config/cloudinary-setup')
 
-import { getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
+import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 import auth from '../middleware/auth.js'
 
 const router = express.Router();
 
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.get('/', getPost);
 router.post('/', auth, createPost);
