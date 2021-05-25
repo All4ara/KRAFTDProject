fa08eb6e6417ff20000ec01f6c02b58fbcc951f3
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-05-25 18:22:44 -0400

    set up dynamic pagination component that connects to the api query aswell

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -8,23 +8,24 @@ import { getPosts } from '../actions/posts';
 import useStyles from './styles';
 
 const Paginate  = ({ page }) => {
+    const { numberOfPages } = useSelector((state) => state.posts);
     const classes = useStyles();
     const dispatch = useDispatch(page);
 
     useEffect(() => {
-       if(page) dispatch(getPosts());
+       if(page) dispatch(getPosts(page));
 
     }, [page])
 
     return (
         <Pagination 
             classes={{ ul: classes.ul }}
-            count={4}
-            page={1}
+            count={numberOfPages}
+            page={Number(page) || 1}
             variant='outlined'
             color='primary'
             renderItem={(item) => (
-                <PaginationItem {...item} component={Link} to={`/posts?page=${1}`} />
+                <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
             )}
         />
     );
