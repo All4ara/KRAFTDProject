3bb95ce563a97b9beba98936ed7a2abd8457fffe
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-05-26 14:21:45 -0400

    Changed loading on posts for soomther delivery

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,12 +6,13 @@ import { useSelector } from 'react-redux'
 
 
 const Posts = ({ setCurrentId }) => {
-    const { posts } = useSelector((state) => state.posts);
+    const { posts, isLoading } = useSelector((state) => state.posts);
     const classes = useStyles();
     
+    if (!posts.length && !isLoading) return 'NO Posts';
 
     return(
-        !posts?.length ? <CircularProgress /> : (
+        isLoading ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {posts.map((post, i, arr) => (
                     <Grid key={post._id} item xs={12} sm={12} md={6} lg={4}>
