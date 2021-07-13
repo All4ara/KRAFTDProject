30d77e192ac5c7712ff14ed71245257db09b28ba
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-07-13 13:58:24 -0400

    fix

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,15 +3,13 @@ import express from 'express';
 // const Post = require('../models/postMessage');
 // const uploader = require('../config/cloudinary-setup')
 
-import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
+import { getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 import auth from '../middleware/auth.js'
 
 const router = express.Router();
 
-router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
-router.get('/:id', getPost);
-
+router.get('/', getPost);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
