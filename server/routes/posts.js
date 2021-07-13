3009d017e08119a0ec03d9ddb935069c543eed75
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-07-13 16:32:16 -0400

    fixed recommented posts in posts details page

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,7 @@
 import express from 'express';
 
-// const Post = require('../models/postMessage');
-// const uploader = require('../config/cloudinary-setup')
-
 import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
+
 import auth from '../middleware/auth.js'
 
 const router = express.Router();
@@ -11,16 +9,9 @@ const router = express.Router();
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.get('/:id', getPost);
-
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
-
-
-
-// router.post('/', uploader.single('file'), (req, res) => {
-//     res.send("ok")
-// })
 
 export default router;
